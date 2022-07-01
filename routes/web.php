e1ea0a5995ebe61454fc9e689d29126a951c5983
hayateeeeee <ooi2172026@stu.oa-hara.ac.jp>
2022-07-01 14:12:27 +0900

    テキストｐ202まで

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ Route::post('hello', 'HelloController@post');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
 
  //--実習用--
  Route::get('jissyu2', 'JissyuController@indax');
