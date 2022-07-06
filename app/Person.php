783c33ca2ed926eab0efdc1a3668598aa001ea16
hayateeeeee <ooi2172026@stu.oa-hara.ac.jp>
2022-07-06 14:03:28 +0900

    テキストｐ254まで

--- app/Person.php
+++ app/Person.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -30,4 +31,10 @@ public function scopeAgeLessThan($query, $n)
       // $builder->where('age', '>', 20);
    //});
 //}
+
+protected static function boot()
+{
+   parent::boot();
+   static::addGlobalScope(new ScopePerson);
+}
 }
