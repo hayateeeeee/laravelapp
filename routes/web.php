a5034f2c1b9affa60a19add02554c1eccdcf73c1
hayateeeeee <ooi2172026@stu.oa-hara.ac.jp>
2022-07-08 12:33:59 +0900

    実習5＿2

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,12 @@ Route::post('jissyu4_3', 'Jissyu4_3Controller@post');
 
 Route::get('jissyu10', 'Jissyu5_1Controller@index');
 Route::get('jissyu10/show', 'Jissyu5_1Controller@show');
+
+Route::get('jissyu11', 'Jissyu5_2Controller@index');
+Route::get('jissyu11/show', 'Jissyu5_2Controller@show');
+Route::get('jissyu11/add', 'Jissyu5_2Controller@add');
+Route::post('jissyu11/create', 'Jissyu5_2Controller@create');
+Route::get('jissyu11/edit', 'Jissyu5_2Controller@edit');
+Route::post('jissyu11/update', 'Jissyu5_2Controller@update');
+Route::get('jissyu11/del', 'Jissyu5_2Controller@del');
+Route::post('jissyu11/remove', 'Jissyu5_2Controller@remove');
